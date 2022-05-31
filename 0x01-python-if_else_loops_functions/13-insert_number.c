2c3d4655ad76b56b7080ce8ed9a7363dbdff2c07
Amin133 <umaraminaabdulmumin@gmial.com>
2022-05-31 16:50:51 -0700

    update fix

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -3,44 +3,35 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stddef.h>
+
 /**
  * insert_node - insert a number sorted in a list
  * @head: data type double ointer of list
  * @number: data type int number to be added
  * Return: 0 if no cycle || 1 if there is cycle
  */
+
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *new;
-	listint_t *current = *head;
+	listint_t *node = *head, *new;
 
 	new = malloc(sizeof(listint_t));
-	if (!new)
+	if (new == NULL)
 		return (NULL);
 	new->n = number;
-	new->next = NULL;
-	if (*head == NULL)
+
+	if (node == NULL || node->n >= number)
 	{
-		*head = new;							       }
-	else
-	{
-		while (current->next != NULL)
-		{
-			if (number >= current->n && number < current->next->n)
-			{
-				new->next = current->next;
-				current->next = new;
-				return (new);
-			}
-			else if (number <= current->n && number < current->next->n)
-			{
-				new->next = current;
-				*head = new;
-				return (new);
-			}
-			current = current->next;
-		}
-		current->next = new;
+		new->next = node;
+		*head = new;
+		return (new);
 	}
+
+	while (node && node->next && node->next->n < number)
+		node = node->next;
+
+	new->next = node->next;
+	node->next = new;
+
 	return (new);
 }
