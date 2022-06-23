cd88ecff40da786c297e65d11cce1666ee1a3888
Umar Amina Abdulmumin <umaraminaabdulmumin@gmail.com>
2022-06-23 04:17:21 -0700

    an empty class Square that defines a square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -2,11 +2,5 @@
 
 
 class Square:
-    """An empty class Square."""
+    """An empty class Square"""
     pass
-
-
-if __name__ == '__main__':
-    my_square = Square()
-    print(type(my_square))
-    print(my_square.__dict__)
